21377ff6c74fe0fe0eac2f89ad49152b4a10c73c
pizocristian <pizocristian@gmail.com>
2020-10-09 21:14:44 -0500

    proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
     
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,18 +9,55 @@ import  {PersonasService} from '../services/personas.service';
 })
 export class HomeComponent implements OnInit {
 
-  personas:Personas[];
-
+  personas: any;
+  nombreUsuario:any = null;
+  persona:any;
+  ocultar:boolean;
+  mostrar:boolean;
   constructor(private servicePersonas: PersonasService){
-    this.llamarServicio();
+
+    this.servicePersonas.obtenerPersonas().subscribe((personas:Personas[])=>{
+ 
+      for(var i = 0; i < personas.length; i += 1){
+         var result: any = personas[i];
+        if(result.first_name === "Gretal" ){
+          console.log(result);
+          this.mostrar=true;
+          return this.persona =result;
+        
+        }}
+        
+    });
   }
   ngOnInit(){
-  
+    this.llamarServicio();
   }
   llamarServicio(){
     this.servicePersonas.obtenerPersonas().subscribe((personas:Personas[])=>{
       console.log(personas)
       this.personas = personas;
     });
+  }
+  public buscar(){
+    
+    this.servicePersonas.obtenerPersonas().subscribe((personas:Personas[])=>{
+      /* console.log(personas)
+      this.personas = personas; */
+      if(this.nombreUsuario ==''){
+        this.mostrar=true;
+      }else{
+      for(var i = 0; i < personas.length; i += 1){
+         var result: any = personas[i];
+        if(result.first_name === this.nombreUsuario ){
+          console.log(result);
+          this.mostrar=false;
+          this.nombreUsuario ="";
+          return this.persona =result;
+        
+        }}
+      }
+    });
+
+  // this.nombreUsuario ="";
   }
 }
